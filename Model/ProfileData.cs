8bf50a974d5f3455978b22cc154a5b33848231e2
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-06-02 12:18:47 -0400

    combine profile data to hold player data stuff

--- Model/ProfileData.cs
+++ Model/ProfileData.cs
@@ -5,6 +5,17 @@ public record class ProfileData
     public required string PlayerId { get; set; }
     public required DisplayName DisplayName { get; set; }
     public required string BannerItemId { get; set; }
+    public required string PreSprayItemId { get; set; }
+    public required string MatchSprayItemId { get; set; }
+    public required string PostSprayItemId { get; set; }
+    public required string AttackerOutfitLoadoutId { get; set; }
+    public required string DefenderOutfitLoadoutId { get; set; }
+    public required string AttackerWeaponLoadoutId { get; set; }
+    public required string DefenderWeaponLoadoutId { get; set; }
+    public required DateTimeOffset LastUpdated { get; set; }
+    public required DateTimeOffset NextNewDayRollover { get; set; }
+    public required DateTimeOffset LastLogin { get; set; }
+    public required Int32 PlayerFlags { get; set; }
     public required Int64 CrewScore { get; set; }
     public required Int32 CurrentSoloRank { get; set; } // todo enum
     public required Int32 HighestTeamRank { get; set; } // todo enum
--- Persistence/InitSQL/1/CreateProfileData.sql
+++ Persistence/InitSQL/1/CreateProfileData.sql
@@ -2,6 +2,17 @@ CREATE TABLE IF NOT EXISTS profile_data (
 	player_id UUID PRIMARY KEY,
 	display_name DisplayName NOT NULL,
 	banner_item_id TEXT NOT NULL,
+	pre_spray_item_id TEXT NOT NULL,
+	match_spray_item_id TEXT NOT NULL,
+	post_spray_item_id TEXT NOT NULL,
+	attacker_outfit_loadout_id TEXT NOT NULL,
+	defender_outfit_loadout_id TEXT NOT NULL,
+	attacker_weapon_loadout_id TEXT NOT NULL,
+	defender_weapon_loadout_id TEXT NOT NULL,
+	last_updated TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	next_new_day_rollover TIMESTAMPTZ NOT NULL,
+	last_login TIMESTAMPTZ NOT NULL,
+	player_flags INT NOT NULL,
 	crew_score BIGINT NOT NULL,
 	current_solo_rank INT NOT NULL,
 	highest_team_rank INT NOT NULL,
@@ -12,3 +23,14 @@ CREATE TABLE IF NOT EXISTS profile_data (
 		(display_name).discriminator IS NOT NULL
 	)
 );
+
+CREATE OR REPLACE FUNCTION profile_data_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+	NEW.last_updated = NOW();
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS trigger_profile_set_tz ON profile_data;
+CREATE TRIGGER trigger_profile_set_tz BEFORE UPDATE ON profile_data FOR EACH ROW EXECUTE FUNCTION profile_data_set_timestamp();
