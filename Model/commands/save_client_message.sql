3826eb96eb9c4f9e237058e8aeb808c086e2e93c
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-06-30 16:44:22 -0400

    Add ClientM

---
+++ Model/commands/save_client_message.sql
@@ -0,0 +1,39 @@
+INSERT INTO client_messages (
+	message_id,
+	player_id,
+	message_type,
+	message_sender,
+	message_sender_type,
+	campaign_id,
+	message_title,
+	message_body,
+	item_attachment_catalog_id,
+	sent_time,
+	read_time,
+	expiration_time
+) VALUES (
+	@message_id,
+	@player_id,
+	@message_type,
+	@message_sender,
+	@message_sender_type,
+	@campaign_id,
+	@message_title,
+	@message_body,
+	@item_attachment_catalog_id,
+	@sent_time,
+	@read_time,
+	@expiration_time
+) ON CONFLICT (message_id)
+DO UPDATE SET
+	player_id = EXCLUDED.player_id,
+	message_type = EXCLUDED.message_type,
+	message_sender = EXCLUDED.message_sender,
+	message_sender_type = EXCLUDED.message_sender_type,
+	campaign_id = EXCLUDED.campaign_id,
+	message_title = EXCLUDED.message_title,
+	message_body = EXCLUDED.message_body,
+	item_attachment_catalog_id = EXCLUDED.item_attachment_catalog_id,
+	sent_time = EXCLUDED.sent_time,
+	read_time = EXCLUDED.read_time,
+	expiration_time = EXCLUDED.expiration_time;
