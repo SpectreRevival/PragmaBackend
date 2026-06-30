a43c2abd147328fbb2ff9d525e342f374e237491
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-06-30 11:06:13 -0400

    Fix the PostSendNotification thing

--- Processors/SpectreWebsocketMessage.cs
+++ Processors/SpectreWebsocketMessage.cs
@@ -6,18 +6,20 @@ namespace Processors;
 public class SpectreWebsocketMessage
 {
     private readonly string _data;
+    private readonly WebsocketNotification[] _postSendNotifications;
 
-    private SpectreWebsocketMessage(string data)
+    private SpectreWebsocketMessage(string data, WebsocketNotification[] postSendNotifications)
     {
         _data = data;
+        _postSendNotifications = postSendNotifications;
     }
 
-    public static SpectreWebsocketMessage From(string data)
+    public static SpectreWebsocketMessage From(string data, WebsocketNotification[]? postSendNotifications = null)
     {
-        return new SpectreWebsocketMessage(data);
+        return new SpectreWebsocketMessage(data, postSendNotifications ?? []);
     }
 
-    public static SpectreWebsocketMessage From(IMessage protoMessage)
+    public static SpectreWebsocketMessage From(IMessage protoMessage, WebsocketNotification[]? postSendNotifications = null)
     {
         JsonFormatter outFormatter = new(
     new JsonFormatter.Settings(true)
@@ -26,16 +28,21 @@ public class SpectreWebsocketMessage
     .WithIndentation("")
     .WithPreserveProtoFieldNames(true)
 );
-        return new SpectreWebsocketMessage(outFormatter.Format(protoMessage));
+        return new SpectreWebsocketMessage(outFormatter.Format(protoMessage), postSendNotifications ?? []);
     }
 
-    public static SpectreWebsocketMessage From(JsonObject json)
+    public static SpectreWebsocketMessage From(JsonObject json, WebsocketNotification[]? postSendNotifications = null)
     {
-        return new SpectreWebsocketMessage(json.ToJsonString());
+        return new SpectreWebsocketMessage(json.ToJsonString(), postSendNotifications ?? []);
     }
 
     public string GetData()
     {
         return _data;
+    }
+
+    public WebsocketNotification[] GetNotifications()
+    {
+        return _postSendNotifications;
     }
 }
