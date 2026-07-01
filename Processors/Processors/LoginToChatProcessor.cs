9c3f3da59e80255cf46f710c555b43ba0c32dff1
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-07-01 11:46:33 -0400

    Port LoginToCh

---
+++ Processors/Processors/LoginToChatProcessor.cs
@@ -0,0 +1,32 @@
+﻿using Packets;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Processors.Processors;
+
+public class LoginToChatProcessor : WebsocketPacketProcessor
+{
+    [SetsRequiredMembers]
+    public LoginToChatProcessor(SpectreRpcType rpcType) : base(rpcType)
+    {
+    }
+
+    public static SpectreRpcType GetRpcType()
+    {
+        return new SpectreRpcType("MtnChatServiceRpc.LoginToChatV2Request");
+    }
+
+    public override async Task<SpectreWebsocketMessage> ProcessPacket(SpectreWebsocketRequest Packet, SpectreWebsocket ConnectionHandler)
+    {
+        LoginToChatResponse res = new();
+        string loginToken = VivoxTokenGenerator.GenerateToken(ConnectionHandler.PlayerId, VivoxTokenAction.LOGIN, "");
+        string joinToken = VivoxTokenGenerator.GenerateToken(ConnectionHandler.PlayerId, VivoxTokenAction.JOIN, "global");
+        res.Success = true;
+        res.Token = loginToken;
+        res.Generalchatroom = "global";
+        res.Generalchattoken = joinToken;
+        res.Chatserver = VivoxTokenGenerator.server;
+        res.Chatdomain = VivoxTokenGenerator.domain;
+        res.Chatissuer = VivoxTokenGenerator.issuer;
+        return SpectreWebsocketMessage.From(res);
+    }
+}
