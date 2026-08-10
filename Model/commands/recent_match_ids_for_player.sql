99e657115a3d90e4e1cf33b8e1cb8c350497b190
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-08-10 12:33:25 -0400

    fix some more sql commands

--- Model/commands/recent_match_ids_for_player.sql
+++ Model/commands/recent_match_ids_for_player.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT MatchHistory.match_id FROM match_history MatchHistory 
+SELECT MatchHistory.match_id FROM match_history MatchHistory 
 INNER JOIN match_history_player_data PlayerData ON MatchHistory.match_id = PlayerData.match_id
 WHERE MatchHistory.match_date BETWEEN @start_date AND @end_date
 AND PlayerData.player_id = @player_id
